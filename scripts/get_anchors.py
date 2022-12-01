1160fa7b7759e9d416ff17574dd0c1dd5eda3d4e
liuk6 <liuk6@xiaopeng.com>
2022-12-01 10:11:38 +0800

    Update anchor clustering

--- scripts/get_anchors.py
+++ scripts/get_anchors.py
@@ -1,16 +1,17 @@
 """Get anchor trajectories with K-means."""
 import torch
 import numpy as np
+import polars as pl
 import matplotlib.pyplot as plt
 
-from sklearn.cluster import KMeans
+from tslearn.clustering import TimeSeriesKMeans
 from scipy.optimize import linear_sum_assignment
 
 from config.defaults import get_cfg
 from projects.wayformer.feature_generator import WaymoFeatureGenerator
 from projects.wayformer.scenario_generator import WaymoScenarioGenerator
 
-N = 200
+N = 500
 
 
 def vis_tracks(tracks):
@@ -32,8 +33,8 @@ def get_anchors(M):
         idx += 1
         if idx == N:
             break
-        # Use focused tracks to generate ref points
-        scenario.tracks = scenario.focused_tracks
+        scenario.tracks = [x for x in scenario.tracks if x.filter(
+            pl.col("is_valid")).shape[0] == len(scenario.timestamps)]
         agent_feats = fg.agent_feats(scenario, track_id)
         if agent_feats is None:
             continue
@@ -41,15 +42,12 @@ def get_anchors(M):
 
     # K-means.
     Xs = np.stack(tracks, 0)  # [N,80,2]
-    endpoints = Xs[:, -1, :]  # [N,2]
+    km = TimeSeriesKMeans(n_clusters=M, random_state=0).fit(Xs)
+    centroids = km.cluster_centers_  # [M,T,2]
+    print(centroids.shape, Xs.shape)
 
-    D = Xs.shape[-1]
-    kmeans = KMeans(n_clusters=M, random_state=0).fit(endpoints)
-    centroids = kmeans.cluster_centers_.reshape(M, D)  # [M,2]
-    centroids = torch.from_numpy(centroids)
-
-    dists = centroids[:, None, :] - endpoints[None, :, :]  # [M,N,2]
-    dists = (dists**2).sum(-1)  # [M,N]
+    dists = centroids[:, None, :, :] - Xs[None, :, :, :]  # [M,N,T,2]
+    dists = (dists**2).sum(axis=(2, 3))  # [M,N]
     row_ind, col_ind = linear_sum_assignment(dists)
     anchors = torch.from_numpy(Xs[col_ind])
     return anchors
